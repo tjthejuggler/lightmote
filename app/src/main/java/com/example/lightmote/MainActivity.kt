419e545c451a482695e3cb4a434d1c70596d6978
Timothy Ross <tjthejuggler@gmail.com>
2020-11-17 20:20:58 -0600

    transitioning over to color wheels from buttons

--- app/src/main/java/com/example/lightmote/MainActivity.kt
+++ app/src/main/java/com/example/lightmote/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.lightmote
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.flask.colorpicker.ColorPickerView
+import com.flask.colorpicker.OnColorChangedListener
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
@@ -44,9 +46,9 @@ class MainActivity : AppCompatActivity() {
         "192.168.43.45"
     )
 
-    fun sendColorChange(ip: String, color: ByteArray, index: Int){
+    fun sendColorChange(ip: String, color: ByteArray, buttonIndex: Int){
         val tvHello=findViewById(R.id.mytextView) as TextView;
-        tvHello.text= colors[index].get(0).toString() + colors[index].get(1).toString() + colors[index].get(2).toString()
+        tvHello.text= buttonIndex.toString()
         val buffer = ByteArray(12)
         val buf: ByteBuffer =
             ByteBuffer.wrap(buffer)
@@ -64,66 +66,104 @@ class MainActivity : AppCompatActivity() {
         )
         val datagramSocket = DatagramSocket()
         thread{datagramSocket.send(packet)}
+
+
     }
+
+
+
 
     private val myListener: View.OnClickListener = object : View.OnClickListener {
         override fun onClick(v: View) {
+            Log.d("onClick", "onClick");
             val tag: Any = v.getTag()
             val buttonIndex = tag as Int
             var ipIndex: Int = 0
-            when {
-                buttonIndex < 7 -> ipIndex = 0
-                buttonIndex in 7..13 -> ipIndex = 1
-                buttonIndex in 14..20 -> ipIndex = 2
-            }
-            sendColorChange(ipAddresses[ipIndex], colors[buttonIndex], buttonIndex)
-            // Do something depending on the value of the tag
+//            when {
+//                buttonIndex < 7 -> ipIndex = 0
+//                buttonIndex in 7..13 -> ipIndex = 1
+//                buttonIndex in 14..20 -> ipIndex = 2
+//            }
+            sendColorChange(ipAddresses[buttonIndex], colors[buttonIndex], buttonIndex)
         }
     }
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("onCreate", "onCreate");
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val buttons = arrayOf(
-            findViewById(R.id.btnA1),
-            findViewById(R.id.btnA2),
-            findViewById(R.id.btnA3),
-            findViewById(R.id.btnA4),
-            findViewById(R.id.btnA5),
-            findViewById(R.id.btnA6),
-            findViewById(R.id.btnA7),
-            findViewById(R.id.btnB1),
-            findViewById(R.id.btnB2),
-            findViewById(R.id.btnB3),
-            findViewById(R.id.btnB4),
-            findViewById(R.id.btnB5),
-            findViewById(R.id.btnB6),
-            findViewById(R.id.btnB7),
-            findViewById(R.id.btnC1),
-            findViewById(R.id.btnC2),
-            findViewById(R.id.btnC3),
-            findViewById(R.id.btnC4),
-            findViewById(R.id.btnC5),
-            findViewById(R.id.btnC6),
-            findViewById<Button>(R.id.btnC7)
+        val colorwheels = arrayOf(
+            findViewById(R.id.color_picker_view),
+            findViewById(R.id.color_picker_view2),
+            findViewById<ColorPickerView>(R.id.color_picker_view3)
         )
 
-        for (i in 0..20) {
-            buttons[i].setOnClickListener(myListener)
-            buttons[i].tag = i
+        //todo
+        //hook up this colorchangelistener to ever wheel
+        //make them change ball colors
+
+        colorwheels[0].addOnColorChangedListener(OnColorChangedListener { selectedColor -> // Handle on color change
+            Log.d(
+                "ColorPicker",
+                "onColorChanged: 0x" + Integer.toHexString(selectedColor)
+            )
+        })
+
+        for (i in 0..2) {
+            colorwheels[i].setOnClickListener(myListener)
+            colorwheels[i].tag = i
+//            colorwheels[i].setBackgroundColor(Color.rgb(colors[i][0].toInt(),colors[i][1].toInt(),colors[i][2].toInt()))
+
         }
+//        val buttons = arrayOf(
+//            findViewById(R.id.btnA1),
+//            findViewById(R.id.btnA2),
+//            findViewById(R.id.btnA3),
+//            findViewById(R.id.btnA4),
+//            findViewById(R.id.btnA5),
+//            findViewById(R.id.btnA6),
+//            findViewById(R.id.btnA7),
+//            findViewById(R.id.btnB1),
+//            findViewById(R.id.btnB2),
+//            findViewById(R.id.btnB3),
+//            findViewById(R.id.btnB4),
+//            findViewById(R.id.btnB5),
+//            findViewById(R.id.btnB6),
+//            findViewById(R.id.btnB7),
+//            findViewById(R.id.btnC1),
+//            findViewById(R.id.btnC2),
+//            findViewById(R.id.btnC3),
+//            findViewById(R.id.btnC4),
+//            findViewById(R.id.btnC5),
+//            findViewById(R.id.btnC6),
+//            findViewById<Button>(R.id.btnC7)
+//        )
+//
+//        for (i in 0..20) {
+//            buttons[i].setOnClickListener(myListener)
+//            buttons[i].tag = i
+//            buttons[i].setBackgroundColor(Color.rgb(colors[i][0].toInt(),colors[i][1].toInt(),colors[i][2].toInt()))
+//            buttons[i].visibility = View.INVISIBLE
+//        }
+
+
     }
 }
 
 //// TODO: 11/17/20 make a grid of buttons
 //        make the grid of buttons
+//          make color widget control ball colors
+//          make 3 color widgets
 //
 //        make long hold on buttons go into color select mode
 //        make normal click change the color of the ball on that row
 //        make a settings buton to input the IP of the balls for each row
 //            or make a button on each row that opens an input for putting in the IP
 //        a way to power off the balls
+//        a way to save button colors
+//          a way to return to default colors
 //
 //        determine if it is better to use their built in sequence system or make my own
 //        a way to record sequences
